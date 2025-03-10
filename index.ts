6f86193ab1572e4fd0ae4b584448b2971b3a0f32
William McDermott <williammcdermott@MacBookPro.lan>
2025-03-09 20:29:31 -0500

    Write report on todo rather than just displaying json

--- index.ts
+++ index.ts
@@ -2,6 +2,18 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
 axios.get(url).then(response => {
-    console.log(response.data);
+    const todo = response.data as Todo;
+    const { id, title, completed } = todo;
+    console.log(`
+        The Todo with ID: ${id}
+        Has a title of: ${title}
+        Is it completed? ${completed}
+    `);
 });
