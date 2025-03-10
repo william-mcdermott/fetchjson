8a8fd6d245436a6b345d14bf6ae26fa81d91d679
William McDermott <williammcdermott@MacBookPro.lan>
2025-03-09 21:31:33 -0500

    new function to log todo info with types

--- index.ts
+++ index.ts
@@ -11,9 +11,13 @@ interface Todo {
 axios.get(url).then(response => {
     const todo = response.data as Todo;
     const { id, title, completed } = todo;
+    logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
     console.log(`
         The Todo with ID: ${id}
         Has a title of: ${title}
         Is it completed? ${completed}
     `);
-});
+};
